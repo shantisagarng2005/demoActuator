b1a35012ff1edb769627303f46284da7d53cc3ef
shant <shant@LAPTOP-T2Q5T8LR>
2021-11-06 21:39:03 +0530

    Code promotion initial

---
+++ src/main/java/com/example/demo/TempTesting.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+interface DefaultMethodInterface
+{
+	default void sayHello()
+	{
+		System.out.println("Hello");
+	}
+}
+@FunctionalInterface
+interface Summation extends DefaultMethodInterface
+{
+	int sum(int i,int j);
+}
+
+public class TempTesting {
+public static int methodRefSum(int i, int j)
+{
+	return i+j;
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Summation sum=TempTesting::methodRefSum;
+		System.out.println("Sum::-"+sum.sum(1, 2));
+		sum.sayHello();
+		
+		List<String> ls=new ArrayList<>();
+		ls.add("sagar");
+		ls.add("vijay");
+		ls.add("uday");
+		ls.add("ajay");
+		Stream<String> filteredList=ls.stream().filter(p->(!p.equals("vijay")));
+		filteredList.forEach((name)->{System.out.println(name);});
+		System.out.println("------------------------");
+		Collections.sort(ls, (a,b)->{return a.toString().compareTo(b.toString());});
+		ls.forEach((item)->System.out.println(item));
+	}
+
+}
